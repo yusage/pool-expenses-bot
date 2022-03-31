188078edd10120c6d213d2c7eed0a1f413bf88a0
yusage <iu.tkachenko@gmail.com>
2022-03-31 22:49:05 +0300

    express added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 const { Telegraf } = require('telegraf');
+const express = require('express');
 require('dotenv').config();
 const setConnector = require('./connectors/setConnector');
 const installPublicBotCommands = require('./telegraf/middleware/publicCommands');
 const installHiddenBotCommands = require('./telegraf/middleware/hiddenCommand');
+
+const port = process.env.PORT || 3000;
+const expressApp = express();
 
 const botToken = process.env.BOT_TOKEN;
 const bot = new Telegraf(botToken);
 
 
 async function main () {
+
+    expressApp.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+    expressApp.listen(port, () => {
+        console.log(`Listening on port ${port}`);
+    });
+
     bot.use(await setConnector('mongoose'));
     installPublicBotCommands(bot);
     installHiddenBotCommands(bot);
