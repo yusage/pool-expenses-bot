871315c60a3396aacb0a2c903bc70ceb2eb55149
yusage <iu.tkachenko@gmail.com>
2022-04-02 16:12:12 +0300

    added a request to app webpage each 20 minutes

--- src/index.js
+++ src/index.js
@@ -9,8 +9,14 @@ const installHiddenBotCommands = require('./telegraf/middleware/hiddenCommand');
 const botToken = process.env.BOT_TOKEN;
 const bot = new Telegraf(botToken);
 
+var http = require('http');
+
 
 async function main () {
+    setInterval(function() {
+        console.log('accessing app webpage...');
+        http.get('http://yusage-pool-expense-bot.herokuapp.com');
+    }, 1200000); // every 20 minutes (1200000=1000*60*20)
     runExpress();
     bot.use(await setConnector('mongoose'));
     installPublicBotCommands(bot);
