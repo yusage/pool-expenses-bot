4e14891087dd2978d4642f89ed1f23e5d94bdd16
yusage <iu.tkachenko@gmail.com>
2022-04-11 21:48:57 +0300

    Express added to meet PORT listening Heroku requirement

--- src/utils/keepHerokuActive.js
+++ src/utils/keepHerokuActive.js
@@ -1,10 +1,16 @@
-var http = require('http');
+const http = require('http');
+const express = require('express');
 
 function keepHerokuActive (timeoutMinutes) {
-    http.get('http://yusage-pool-expense-bot.herokuapp.com');
     setInterval(function() {
         http.get('http://yusage-pool-expense-bot.herokuapp.com');
     }, 1000*60*timeoutMinutes);
+
+    const app = express();
+    const port = process.env.PORT;
+    app.listen(port, () => {
+        console.log('Listening to port ' + port);
+    });
 }
 
 module.exports = keepHerokuActive;
