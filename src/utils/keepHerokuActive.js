598d1749d6dd5eb3d6670ab96177c0cfd2abee92
yusage <iu.tkachenko@gmail.com>
2022-04-11 21:29:14 +0300

    initial heroku server ping added

--- src/utils/keepHerokuActive.js
+++ src/utils/keepHerokuActive.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 function keepHerokuActive (timeoutMinutes) {
+    http.get('http://yusage-pool-expense-bot.herokuapp.com');
     setInterval(function() {
         http.get('http://yusage-pool-expense-bot.herokuapp.com');
     }, 1000*60*timeoutMinutes);
